750b88821c8253727b5540ff8fb744ba2cedf559
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-03 23:56:40 +0530

    Wanderlust

--- app.js
+++ app.js
@@ -38,6 +38,19 @@ app.get("/", (req, res) => {
 });
 
 
+// Validation for Schema (Middleware)
+const validateListing = (req, res) => {
+  let {error} = listingSchema.validate(req.body);
+    if(error){
+      let errMsg = error.details.map((el)=> el.message).join(",");
+      throw new ExpressError(400, errMsg);
+    }
+    else{
+      next();
+    }
+};
+
+
 //Index Route
 app.get("/listings", wrapAsync (async (req, res) => {
   const allListings = await Listing.find({});
@@ -60,12 +73,7 @@ app.get("/listings/:id", wrapAsync (async (req, res) => {
 
 
 //Create Route
-app.post("/listings", wrapAsync (async (req, res, next) => {
-    let result = listingSchema.validate(req.body);
-    console.log(result);
-    if(result.error){
-      throw new ExpressError(400, result.error);
-    }
+app.post("/listings", validateListing, wrapAsync (async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -82,10 +90,7 @@ app.get("/listings/:id/edit", wrapAsync (async (req, res) => {
 
 
 //Update Route
-app.put("/listings/:id", wrapAsync (async (req, res) => {
-  if(!req.body.listing){
-    throw new ExpressError("Invalid input", 400);
-  }
+app.put("/listings/:id", validateListing, wrapAsync (async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
