77d01757614e8727a0b7e1e4a3557b9a4be55e88
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-11 01:28:23 +0530

    Wanderlust

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 
@@ -60,10 +63,35 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 
+
+// Passport middleware to initialize and configure Passport
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
+});
+
+
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    email: "student@example.com",
+    username: "delta-student",
+  });
+  let registeredUser = await User.register(fakeUser, "helloworld");
+  res.send(registeredUser);
 });
 
 
