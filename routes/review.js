f58819e3114618143871786953aac3294782757d
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-14 03:36:28 +0530

    Wanderlust

--- routes/review.js
+++ routes/review.js
@@ -3,19 +3,18 @@ const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middleware.js");
+const {validateReview, isLoggedIn} = require("../middleware.js");
 
 
 
 
 // Reviews
 // Post Review Route
-router.post("/", validateReview, wrapAsync (async (req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync (async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-  
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
-  
     await newReview.save();
     await listing.save();
     req.flash("success", "New Review Created!");
