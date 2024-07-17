1c31299822266dbb910b02ff180ea0c4b644be35
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-18 03:06:37 +0530

    Wanderlust

--- routes/listing.js
+++ routes/listing.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const upload = multer({dest: "uploads/"});
 
 
 
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn, validateListing, wrapAsync(listingController.createListings));
+// .post(isLoggedIn, validateListing, wrapAsync(listingController.createListings));
+.post(upload.single("listing[image]"), (req, res) => {
+    res.send(req.file);
+});
 
 
 //New Route
