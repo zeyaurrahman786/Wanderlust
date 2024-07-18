24b426ccd56a022593c4d575ea909bb57c9ca75a
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-18 13:56:29 +0530

    Wanderlust

--- controllers/listings.js
+++ controllers/listings.js
@@ -49,14 +49,23 @@ module.exports.renderEditForm = async (req, res) => {
       req.flash("error", "Listing you requested for does not exist!");
       res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
+
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 
 
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    if(typeof req.file !== "undefined"){
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = {url, filename};
+    await listing.save();
+    }
     req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 };
--- routes/listing.js
+++ routes/listing.js
@@ -22,7 +22,7 @@ router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.route("/:id")
 .get(wrapAsync(listingController.showListing))
-.put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+.put(isLoggedIn, isOwner, upload.single("listing[image]"), validateListing, wrapAsync(listingController.updateListing))
 .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 
