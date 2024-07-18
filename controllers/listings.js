99feb4b031414ad8d186b02a74cfcfbdbc7ede7d
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-07-18 12:19:40 +0530

    Wanderlust

--- controllers/listings.js
+++ controllers/listings.js
@@ -29,8 +29,12 @@ module.exports.showListing = async (req, res) => {
 
 
 module.exports.createListings = async (req, res, next) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
--- routes/listing.js
+++ routes/listing.js
@@ -5,17 +5,15 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer = require("multer");
-const upload = multer({dest: "uploads/"});
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 
 
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-// .post(isLoggedIn, validateListing, wrapAsync(listingController.createListings));
-.post(upload.single("listing[image]"), (req, res) => {
-    res.send(req.file);
-});
+.post(isLoggedIn,  upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListings));
 
 
 //New Route
